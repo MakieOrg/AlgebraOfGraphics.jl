ea74f5f9151d040672123f869c408bf11a08c8e9
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2020-03-12 21:40:36 +0000

    switch to normal operations (#3)

--- src/algebra.jl
+++ src/algebra.jl
@@ -1,9 +1,11 @@
+abstract type AbstractComposite <: AbstractElement end
+
 function _append(t::Tuple, el)
     i = findlast(x -> applicable(combine, x, el), t)
     i === nothing ? (t..., el) : setindex(t, combine(t[i], el), i)
 end
 
-struct Product{T<:Tuple}
+struct Product{T<:Tuple} <: AbstractComposite
     elements::T
     Product(args...) = new{typeof(args)}(args)
 end
@@ -12,8 +14,10 @@ Product(l::Product) = l
 append(l::Product, x) = Product(_append(l.elements, x)...)
 append(l::Product, m::Product) = foldl(append, m.elements, init=l)
 
-⊗(a, b) = applicable(combine, a, b) ? combine(a, b) : Product(a) ⊗ Product(b)
-⊗(a::Product, b::Product) = append(a, b)
+function *(a::AbstractElement, b::AbstractElement)
+    applicable(combine, a, b) ? combine(a, b) : Product(a) * Product(b)
+end
+*(a::Product, b::Product) = append(a, b)
 
 function get_type(p::Product, ::Type{T}) where T
     vals = p.elements
@@ -21,20 +25,20 @@ function get_type(p::Product, ::Type{T}) where T
     i === nothing ? T() : vals[i]
 end
 
-struct Sum{T<:Tuple}
+struct Sum{T<:Tuple} <: AbstractComposite
     elements::T
     Sum(args...) = new{typeof(args)}(args)
 end
 Sum(l::Sum) = l
 
-⊗(t::Sum, b) = Sum(map(el -> el ⊗ b, t.elements)...)
-⊗(a, t::Sum) = Sum(map(el -> a ⊗ el, t.elements)...)
-function ⊗(s::Sum, t::Sum)
-    f = (s ⊗ first(t.elements))
-    ls = (s ⊗ Sum(tail(t.elements)...))
-    return f ⊕ ls
+*(t::Sum, b::AbstractElement) = Sum(map(el -> el * b, t.elements)...)
+*(a::AbstractElement, t::Sum) = Sum(map(el -> a * el, t.elements)...)
+function *(s::Sum, t::Sum)
+    f = (s * first(t.elements))
+    ls = (s * Sum(tail(t.elements)...))
+    return f + ls
 end
-⊗(s::Sum, ::Sum{Tuple{}}) = Sum()
+*(s::Sum, ::Sum{Tuple{}}) = Sum()
 
-⊕(a, b) = Sum(a) ⊕ Sum(b)
-⊕(a::Sum, b::Sum) = Sum(a.elements..., b.elements...)
++(a::AbstractElement, b::AbstractElement) = Sum(a) + Sum(b)
++(a::Sum, b::Sum) = Sum(a.elements..., b.elements...)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,30 @@
 using AlgebraOfGraphics, Test
-using AlgebraOfGraphics: Data, Select, Group, Analysis, Trace, traces, ⊗, ⊕
+using AlgebraOfGraphics: Data,
+                         Select,
+                         Group,
+                         Analysis,
+                         Trace,
+                         traces,
+                         AbstractElement
+
 using RDatasets: dataset
+
+struct Metadata{T} <: AbstractElement
+    x::T
+end
 
 @testset "traces" begin
     mpg = dataset("ggplot2", "mpg")
-    spec = Data(mpg) ⊗ Select(:Cyl, :Hwy) ⊗ Group(color = :Year)
-    s = log ⊕ exp
+    spec = Data(mpg) * Select(:Cyl, :Hwy) * Group(color = :Year)
+    s = Metadata(log) + Metadata(exp)
     ts = traces(s, spec)
-    @test ts[1][1] == (log,)
-    @test ts[2][1] == (exp,)
+    @test ts[1][1] == (Metadata(log),)
+    @test ts[2][1] == (Metadata(exp),)
     @test ts[1][2] isa Vector{<:Trace}
     @test length(ts) == 2
 
     m1, ts1 = traces(Data(mpg), Select(:Cyl, :Hwy), Group(color=:Year))
-    m2, ts2 = traces(Data(mpg) ⊗ Select(:Cyl, :Hwy) ⊗ Group(color=:Year))
+    m2, ts2 = traces(Data(mpg) * Select(:Cyl, :Hwy) * Group(color=:Year))
     @test ts1[1].attributes == ts2[1].attributes
     @test ts1[1].select.args == ts2[1].select.args
     @test ts1[1].select.kwargs == ts2[1].select.kwargs
