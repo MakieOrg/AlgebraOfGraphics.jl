add4fec4e14c5f73054f1b3a343ee395b2d0024e
piever <pietro.vertechi@protonmail.com>
2020-07-11 12:32:17 +0100

    use CairoMakie branch for docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,7 @@
 using Documenter, AlgebraOfGraphics, Literate, Glob
 # avoid font caching warning in docs
+import Pkg
+Pkg.pkg"add https://github.com/JuliaPlots/CairoMakie.jl#pv/update"
 using AbstractPlotting, CairoMakie, MakieLayout
 CairoMakie.activate!()
 scatter(rand(10), rand(10))
