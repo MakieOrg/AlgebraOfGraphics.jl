b14dc259ba8ab767be66b1d4f8d14180c17b4a7b
piever <pietro.vertechi@protonmail.com>
2020-10-15 12:24:19 +0200

    more docs

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,7 @@ makedocs(
          pages = Any[
                      "index.md",
                      "generated/tutorial.md",
+                     "generated/statistics.md",
                      "analyses.md",
                      "generated/internals.md",
                     ]
