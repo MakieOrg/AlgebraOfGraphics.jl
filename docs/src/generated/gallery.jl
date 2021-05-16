79113c57a9fdf7bfb3476b653e62c4d5798f02ea
piever <pietro.vertechi@protonmail.com>
2021-05-16 18:29:56 +0200

    fix footnote

--- docs/src/generated/gallery.jl
+++ docs/src/generated/gallery.jl
@@ -280,9 +280,11 @@ df = (; geometry, group)
 plt = data(df) * visual(Poly) * mapping(:geometry, color = :group)
 draw(plt; axis=(aspect=1,))
 
-# Antarctic coastline[^1].
+# ### Geographic data
 #
-# [^1] 'Gerrish, L., Fretwell, P., & Cooper, P. (2021). Medium resolution vector polygons of the Antarctic coastline (7.4) [Data set]. UK Polar Data Centre, Natural Environment Research Council, UK Research & Innovation. https://doi.org/10.5285/747e63e-9d93-49c2-bafc-cf3d3f8e5afa'
+# Antarctic coastline. Data from the SCAR Antarctic Digital Database[^1].
+#
+# [^1]: Gerrish, L., Fretwell, P., & Cooper, P. (2021). Medium resolution vector polygons of the Antarctic coastline (7.4) [Data set]. UK Polar Data Centre, Natural Environment Research Council, UK Research & Innovation. https://doi.org/10.5285/747e63e-9d93-49c2-bafc-cf3d3f8e5afa
 
 using Downloads, Shapefile, GeoInterface, ZipFile
 
@@ -310,7 +312,7 @@ end
 
 df = (; polygons, t.surface)
 plt = data(df) * mapping(:polygons, color = :surface) * visual(Poly)
-draw(plt)
+draw(plt; axis=(aspect=1,))
 
 # ## New columns on the fly
 
