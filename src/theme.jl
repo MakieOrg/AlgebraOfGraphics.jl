a9b7e3e9da766d117a93052708d21f19508ef2eb
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2021-05-17 14:52:27 +0200

    update naming and order of wong (#176)

--- src/theme.jl
+++ src/theme.jl
@@ -5,12 +5,12 @@
 function default_palettes()
     return (
         color=[
-            RGB(0/255, 114/255, 178/255), # dark blue
+            RGB(0/255, 114/255, 178/255), # blue
             RGB(230/255, 159/255, 0/255), # orange
             RGB(0/255, 158/255, 115/255), # green
-            RGB(204/255, 121/255, 167/255), # pink
-            RGB(213/255, 94/255, 0/255), # red
-            RGB(86/255, 180/255, 233/255), # light blue
+            RGB(204/255, 121/255, 167/255), # reddish purple
+            RGB(86/255, 180/255, 233/255), # sky blue
+            RGB(213/255, 94/255, 0/255), # vermillion
             RGB(240/255, 228/255, 66/255), # yellow
         ],
         marker=[:circle, :utriangle, :cross, :rect, :diamond, :dtriangle, :pentagon, :xcross],
