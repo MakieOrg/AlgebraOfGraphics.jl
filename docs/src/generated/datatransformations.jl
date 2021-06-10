51ec6e6d4cbebc9a53823a5d25784cfb32f8ad70
Rik Huijzer <rikhuijzer@pm.me>
2021-06-10 19:41:12 +0200

    Push ylabel=""

--- docs/src/generated/datatransformations.jl
+++ docs/src/generated/datatransformations.jl
@@ -36,7 +36,7 @@ data(df) * mapping(:x, :y, layout=:z) * histogram(bins=15) |> draw
 df = (x=randn(5000), y=randn(5000), z=rand(["a", "b", "c", "d"], 5000))
 datalimits = ((-2.5, 2.5),)
 xz = data(df) * mapping(:x, layout=:z) * AlgebraOfGraphics.density(; datalimits)
-axis = (; ylabel="density")
+axis = (; ylabel="")
 draw(xz; axis)
 
 #
