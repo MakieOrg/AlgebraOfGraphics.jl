7065950939c5abaa662d38937dd6b825b4007bb3
Fabian Greimel <fabgrei@gmail.com>
2021-10-11 12:15:19 +0200

    add example to gallery
    gallery
    
    
    typo

--- docs/gallery/gallery/layout/faceting.jl
+++ docs/gallery/gallery/layout/faceting.jl
@@ -10,15 +10,46 @@ set_aog_theme!() #src
 
 # ## Facet grid
 
-df = (x=rand(100), y=rand(100), i=rand(["a", "b", "c"], 100), j=rand(["d", "e", "f"], 100))
-plt = data(df) * mapping(:x, :y, col=:i, row=:j)
+df = let
+	N = 100
+	x0 = rand(1:10, N)
+	i = rand(["α", "β"], N)
+	j = rand(["a", "b", "c"], N)
+
+	x = map(zip(x0, j)) do (xx, jj)
+		shift = jj == "a" ? -2.9 : jj == "c" ? 2.9 : 0.0
+		xx + shift
+	end
+
+	y = map(zip(x0, i)) do (xx, ii)
+		shift = ii == "α" ? -3.9 : 3.9
+		xx + 2 + shift + rand()
+	end
+
+	(; x, y, i, j)
+end
+
+plt = data(df) * mapping(:x, :y, row=:i, col=:j)
+
 draw(plt)
+
+# ## Facet grid with all x-axes linked
+
+draw(plt, facet = (; linkxaxes = :all))
+
+# ## Facet grid unlinked x-axes
+
+draw(plt, facet = (; linkxaxes = :none))
 
 # ## Facet wrap
 
 df = (x=rand(100), y=rand(100), l=rand(["a", "b", "c", "d", "e"], 100))
 plt = data(df) * mapping(:x, :y, layout=:l)
 draw(plt)
+
+# ## Facet wrap with unlinked axes
+
+draw(plt, facet = (; linkxaxes = :none, linkyaxes = :none))
 
 # ## Adding traces to only some subplots
 
