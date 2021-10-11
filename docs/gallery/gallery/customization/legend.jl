bc92612987e519d3209153ae3592f4e6ddc60956
Fabian Greimel <fabgrei@gmail.com>
2021-10-11 17:48:13 +0200

    adjust gallery example (has a legend now)

--- docs/gallery/gallery/customization/legend.jl
+++ docs/gallery/gallery/customization/legend.jl
@@ -9,14 +9,14 @@ set_aog_theme!() #src
 
 # To tweak the position and appearance of the legend, simply use the `legend` keyword when plotting. For example
 
-df = (x=rand(100), y=rand(100), z=rand(100))
-layers = linear() + mapping(color=:z)
+df = (x=rand(100), y=rand(100), group=rand(["a looooooong label", "an even loooooonger label", "and one more long label"], 100))
+layers = linear() + mapping(color=:group)
 plt = data(df) * layers * mapping(:x, :y)
-draw(plt, axis=(aspect=1,))
+draw(plt)
 
 #
 
-fg = draw(plt, legend=(position = :top, framevisible = true))
+fg = draw(plt, legend=(position = :top, framevisible = true, padding = 5))
 
 # save cover image #src
 mkpath("assets") #src
