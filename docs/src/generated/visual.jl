cdcac496cb7524942a8aafe1241865705e6d2d6f
David Little <david.frank.little@gmail.com>
2022-01-16 08:27:17 -0500

    Clarify docs for `visual` (#324)
    * clarify docs for `visual`
    
    * fix typo

--- docs/src/generated/visual.jl
+++ docs/src/generated/visual.jl
@@ -3,9 +3,18 @@
 # The function `visual` can be used to give data-independent visual information about the plot
 # (plotting function or attributes).
 
-# The available plotting functions are documented [here](http://makie.juliaplots.org/dev/examples/plotting_functions/).
-# Refer to plotting functions using upper CamelCase for `visual`'s first argument (e.g. `visual(Scatter), visual(BarPlot)`).
-# See the documentation of each plotting function to discover the available attributes.
+# The available plotting functions are documented
+# [here](http://makie.juliaplots.org/dev/examples/plotting_functions/). Refer to
+# plotting functions using upper CamelCase for `visual`'s first argument (e.g.
+# `visual(Scatter), visual(BarPlot)`). See the documentation of each plotting
+# function to discover the available attributes. These attributes can be passed
+# as additional keyword arguments to `visual`, or as part of the `mapping` 
+# you define.
+
+# In fact `visual` can be used for any plotting function that is defined using
+# the `@recipe` macro from Makie. This means that if you have a custom recipe
+# defined in another package you can use it from AlgebraOfGraphics just like any
+# of the plotting functions defined in Makie.
 
 ## Examples
 
