261ef3bb1f156a6e0fea9b5322619de3a1810fe8
onetonfoot <onetonfoot@gmail.com>
2022-01-21 17:26:06 +0800

    add adjust legend title and label order (#320)
    * add adjust legend title and label order
    
    * put labels into variable
    
    * minor formatting
    
    Co-authored-by: Dominic Morgan <dmorgan@pmdcreate.com>
    Co-authored-by: Pietro Vertechi <pietro.vertechi@protonmail.com>

--- docs/gallery/gallery/customization/legend.jl
+++ docs/gallery/gallery/customization/legend.jl
@@ -9,7 +9,8 @@ set_aog_theme!() #src
 
 # To tweak the position and appearance of the legend, simply use the `legend` keyword when plotting. For example
 
-df = (x=rand(100), y=rand(100), group=rand(["a looooooong label", "an even loooooonger label", "and one more long label"], 100))
+labels = ["a looooooong label", "an even loooooonger label", "and one more long label"]
+df = (x=rand(100), y=rand(100), group=rand(labels, 100))
 layers = linear() + mapping(color=:group)
 plt = data(df) * layers * mapping(:x, :y)
 draw(plt)
@@ -17,6 +18,12 @@ draw(plt)
 #
 
 fg = draw(plt, legend=(position=:top, titleposition=:left, framevisible=true, padding=5))
+
+# To adjust the title and order of labels in a legend you can use the pair syntax.
+
+layers = linear() +  mapping(color=:group => sorter(labels) => "Labels")
+plt = data(df) * layers * mapping(:x, :y)
+draw(plt)
 
 # save cover image #src
 mkpath("assets") #src
