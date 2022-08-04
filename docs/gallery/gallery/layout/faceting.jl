3519a289952f599cb33e78712db61a54cd5ca5bf
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-08-04 14:46:51 +0200

    add note about synchronization behavior to docs

--- docs/gallery/gallery/layout/faceting.jl
+++ docs/gallery/gallery/layout/faceting.jl
@@ -75,6 +75,11 @@ draw(plt)
 # If you have too many facets for one figure, you can use `paginate` to split the data into several subsets
 # given a maximum number of plots per layout, row or column.
 
+# Note that pagination is considered an experimental feature.
+# In the current implementation, scales and layouts are not synchronized across pages.
+# This means that, e.g., linked limits on one page are not influenced by limits of other pages.
+# The exact synchronization behavior can be subject to change in non-breaking versions.
+
 df = (x=rand(500), y=rand(500), l=rand(["a", "b", "c", "d", "e", "f", "g", "h"], 500))
 plt = data(df) * mapping(:x, :y, layout=:l)
 pag = paginate(plt, layout = 4)
