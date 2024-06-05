bb3c93829663fc5ad487f0ab8ae773d17f393c5f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-06-05 11:49:19 +0200

    fix frequency

--- src/transformations/frequency.jl
+++ src/transformations/frequency.jl
@@ -12,7 +12,13 @@ to_nothings(v) = fill(nothing, axes(v))
 function (f::FrequencyAnalysis)(input::ProcessedLayer)
     positional = vcat(input.positional, Any[map(to_nothings, first(input.positional))])
     labels = set(input.labels, length(positional) => "count")
-    augmented_input = ProcessedLayer(input; positional, labels)
+    N = length(positional)
+    attributes = if N == 2
+        merge(dictionary([:direction => :y]), input.attributes)
+    else
+        input.attributes
+    end
+    augmented_input = ProcessedLayer(input; positional, labels, attributes)
     return groupreduce(Counter, augmented_input)
 end
 
