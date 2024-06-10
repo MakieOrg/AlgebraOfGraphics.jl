ed7feb613739611040e1056f3e6d8b9a6d65eefc
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-06-10 16:06:22 +0200

    delete test that doesn't make sense anymore

--- test/legend.jl
+++
@@ -1,8 +0,0 @@
-@testset "legend_merging" begin
-    mwe_data = (; t = 1:20, y1 = ones(20), y2 = rand(20))
-    mwe_names = ["f_n", "f_d"]
-    plt = data(mwe_data) *
-        mapping("t", ["y1", "y2"] .=> "y"; color = dims(1) => (i -> mwe_names[i]), marker=dims(1)) *
-        (visual(Lines) + visual(Scatter))
-    @test_throws Makie.MakieCore.InvalidAttributeError draw(plt)
-end
