f5a6be2846a1fd8254649a85dfe741785feb32fe
Documenter.jl <documenter@juliadocs.github.io>
2024-10-07 12:18:02 +0000

    build based on 448d330

--- dev/gallery/gallery/customization/legend.jl
+++ dev/gallery/gallery/customization/legend.jl
@@ -21,4 +21,19 @@ legend!(makie_fig[1, 1], grid; tellheight=false, tellwidth=false, halign=:right,
 
 makie_fig
 
+df = (;
+    x = repeat(1:100, 5),
+    y = reduce(vcat, [[cos(x) for x in range(0, 8pi, length = 100)] .+ 0.3 .* randn.() for _ in 1:5]),
+    group = repeat(1:5, inner = 100),
+)
+
+lin = data(df) *
+    mapping(:x, :y, group = :group => nonnumeric) *
+    visual(Lines, linewidth = 0.3, label = "Lines", legend = (; linewidth = 1.5))
+sca = data(df) *
+    mapping(:x, :y => y -> y + 5, group = :group => nonnumeric) *
+    visual(Scatter, markersize = 3, label = "Scatter", legend = (; markersize = 12))
+
+draw(lin + sca)
+
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
