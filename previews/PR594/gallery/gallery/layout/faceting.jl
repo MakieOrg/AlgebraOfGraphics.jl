2910f2324ecab2c17da392c29041d4393d1defe9
Documenter.jl <documenter@juliadocs.github.io>
2025-03-14 10:37:34 +0000

    delete history

---
+++ previews/PR594/gallery/gallery/layout/faceting.jl
@@ -0,0 +1,64 @@
+using AlgebraOfGraphics, CairoMakie
+
+df = let
+    N = 100
+    x0 = rand(1:10, N)
+    i = rand(["α", "β"], N)
+    j = rand(["a", "b", "c"], N)
+
+    x = map(zip(x0, j)) do (xx, jj)
+        shift = jj == "a" ? -2.9 : jj == "c" ? 2.9 : 0.0
+        xx + shift
+    end
+
+    y = map(zip(x0, i)) do (xx, ii)
+        shift = ii == "α" ? -3.9 : 3.9
+        xx + 2 + shift + rand()
+    end
+
+    (; x, y, i, j)
+end
+
+plt = data(df) * mapping(:x, :y, row=:i, col=:j)
+
+draw(plt)
+
+draw(plt, facet=(; linkxaxes=:minimal, linkyaxes=:minimal))
+
+draw(plt, facet=(; linkxaxes=:none))
+
+df = (x=rand(100), y=rand(100), l=rand(["a", "b", "c", "d", "e"], 100))
+plt = data(df) * mapping(:x, :y, layout=:l)
+draw(plt)
+
+draw(plt, facet=(; linkxaxes=:none, linkyaxes=:none))
+
+draw(plt, scales(Layout = (; palette = [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2)])))
+
+df1 = (x=rand(100), y=rand(100), i=rand(["a", "b", "c"], 100), j=rand(["d", "e", "f"], 100))
+df2 = (x=[0, 1], y=[0.5, 0.5], i=fill("a", 2), j=fill("e", 2))
+layers = data(df1) * visual(Scatter) + data(df2) * visual(Lines)
+fg = draw(layers * mapping(:x, :y, col=:i, row=:j))
+
+df = (x=rand(100), y=rand(100), l=rand([1, 2, 3, 4, 5], 100))
+plt = data(df) * mapping(:x, :y, layout=:l => nonnumeric)
+draw(plt)
+
+df = (
+    x = repeat(1:10, 36),
+    y = cumsum(sin.(range(0, 10pi, 360))),
+    group = repeat(string.("Group ", 1:36), inner = 10),
+    color = 1:360,
+)
+plt = data(df) * mapping(:x, :y, color = :color, layout=:group) * visual(Lines)
+draw(plt)
+
+pag = paginate(plt, layout = 9)
+
+figuregrids = draw(pag)
+
+figuregrids[1]
+
+draw(pag, 4)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
