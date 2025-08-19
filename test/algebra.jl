eb71ae7293652d7b3a7d6a8eaad9283e55ab982f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-08-19 10:35:25 +0200

    add tests

--- test/algebra.jl
+++ test/algebra.jl
@@ -266,3 +266,26 @@ end
     @test e2.named[:align] == aligns[6:10]
     @test e2.named[:color] == colors[6:10]
 end
+
+@testset "hardcoded categoricals work with continuous data" begin
+
+    hardcodeds = [:layout, :col, :row, :group]
+
+    for hardcoded in hardcodeds
+        aes = AlgebraOfGraphics.hardcoded_mapping(hardcoded)
+        @test aes !== nothing
+        fg = draw(mapping(1:3, 1:3; (; hardcoded => [3, 1, 2])...))
+        sc = fg.grid[1].categoricalscales[aes][nothing]
+        @test AlgebraOfGraphics.datavalues(sc) == [1, 2, 3]
+        expected_palette = hardcoded === :layout ? [(1, 1), (1, 2), (2, 1)] : 1:3
+        @test AlgebraOfGraphics.plotvalues(sc) == expected_palette
+    end
+end
+@testset "tuples of columns can be passed without transform func" begin
+    spec = data((; x = 1:4, y = 1:4, a = [2, 1, 1, 2], b = [4, 4, 3, 3])) *
+        mapping(:x, :y, row = (:a, :b))
+    fg = draw(spec)
+    sc = fg.grid[1].categoricalscales[AlgebraOfGraphics.AesRow][nothing]
+    @test AlgebraOfGraphics.datavalues(sc) == [(1, 3), (1, 4), (2, 3), (2, 4)]
+    @test AlgebraOfGraphics.plotvalues(sc) == 1:4
+end
